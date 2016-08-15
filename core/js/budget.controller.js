9288ce83b3f33013e051bedd182e1b84ae70d1ec
Heriberto Basulto <basulto93@live.com>
2016-08-15 10:48:04 -0700

    Few adjustments

--- core/js/budget.controller.js
+++ core/js/budget.controller.js
@@ -25,25 +25,37 @@
         vm.amountLeft =0;;
 
         function addIncome(){
-            vm.incomeList.push(angular.copy(vm.income));
-            console.log(vm.income);
-            //Add up the total amount of income after every input
-            vm.totalIncome += vm.income.amount;
-            vm.amountLeft += vm.income.amount;
+           
+            if(vm.income.amount <= 0 || vm.income.amount === NaN){
+                alert("Please type a valid number!");
+                console.log("if works, income");
+            }else{
+                vm.incomeList.push(angular.copy(vm.income));
 
-            vm.income.name = "";
-            vm.income.amount = ""; 
+                //Add up the total amount of income after every input
+                vm.totalIncome += vm.income.amount;
+                vm.amountLeft += vm.income.amount;
+
+                vm.income.name = "";
+                vm.income.amount = ""; 
+            }
         } 
 
         function addExpense(){
-            vm.expenseList.push(angular.copy(vm.expense));
-            console.log(vm.expense);
+            
+            if(vm.expense.amount <= 0 || vm.expense.amount === NaN){
+                alert("Please type a valid number!");
+                console.log("if works, expense");
+            }else{
+                vm.expenseList.push(angular.copy(vm.expense));
+
             //Add up the total amount of expense after every input
             vm.totalExpense += vm.expense.amount;
             vm.amountLeft -= vm.expense.amount;
 
             vm.expense.name = "";
             vm.expense.amount = "";
+            }
         }
     }
 })();
